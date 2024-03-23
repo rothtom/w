55c9423887521b1905a2922fbe5fe78d049a9a25
rothtom <t.roth1@outlook.com>
2024-03-23 12:53:22 +0100

    completed porject_1

--- 1/wiki/encyclopedia/urls.py
+++ 1/wiki/encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/", views.title, name="title"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("new_entry/", views.new_entry, name="new_entry"),
+    path("random/", views.random, name="random"),
+    path("edit/", views.edit, name="edit")
 ]
--- 1/wiki/encyclopedia/views.py
+++ 1/wiki/encyclopedia/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.middleware import csrf
 import markdown2
-import requests
 import os
+import requests
+from random import choice
 
 from . import util
 
@@ -24,33 +25,73 @@ def index(request):
 
 def title(request, title):
     if entry := util.get_entry(title):
-        if os.path.exists(f"templates/encyclopedia/{title}.html") != True:
-            create_template(title, entry)
-        return render(request, f"templates/encyclopedia/{title}.html")
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": markdown2.markdown(entry)
+        })
     return render(request, "encyclopedia/entry_not_found.html")
 
-def search(request, keyword):
+
+def search(request):
     if request.method != "GET":
-        return HttpResponseRedirect("")
+        return HttpResponseRedirect("../")
+    
+    keyword = request.GET["q"].lower()
     
     if util.get_entry(keyword) != None:
-        return HttpResponseRedirect(f"wiki/{keyword}")
+        return HttpResponseRedirect(f"../wiki/{keyword}")
     
     entries = util.list_entries()
     matches = []
     for entry in entries:
-        if keyword in entry:
+        print(entry)
+        if keyword in entry.lower():
             matches.append(entry)
+
+    if matches == []:
+        return HttpResponseRedirect("../")
     
+    print("results")
     return render(request, "encyclopedia/search_results.html", {
+        "keyword": keyword,
         "results": matches
     })
+
+
+def new_entry(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new_entry.html")
     
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+
+    if title in util.list_entries():
+        return render(request, "encyclopedia/entry_already_exists_error.html", {
+            "title": title
+        })
+    
+    util.save_entry(title, content)
+    
+    return HttpResponseRedirect(f"../wiki/{title}")
 
 
-def create_template(title, entry):
-    path = os.path.join("templates/encyclopedia/", f"{title}.html")
-    with open(path, "w") as f:
-        print("Hello")
-        f.write(entry)
+def random(request):
+    entry = choice(util.list_entries())
+    return HttpResponseRedirect(f"/wiki/{entry}")
 
+def edit(request):
+    if request.method == "GET":
+        title = request.GET.get("title")
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/edit_entry.html", {
+            "title": title,
+            "content": content
+        })
+    
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+
+    util.save_entry(title, content)
+    return HttpResponseRedirect(f"../wiki/{title}")
+
+    

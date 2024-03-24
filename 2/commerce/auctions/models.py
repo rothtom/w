eed105f25637e8bc65ee41e622b0794b2e959fd0
rothtom <t.roth1@outlook.com>
2024-03-24 22:39:21 +0100

    Implemented create view, models and started index view

--- 2/commerce/auctions/models.py
+++ 2/commerce/auctions/models.py
@@ -4,3 +4,28 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Item(models.Model):
+    title = models.CharField(max_length=48)
+    description = models.CharField(max_length=128)
+    picture_link = models.URLField(blank=True)
+    category = models.CharField(max_length=16, blank=True)
+    starting_bid = models.IntegerField(max_length=12)
+
+class Listing(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    
+
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.CharField(max_length=12)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.bidder} bidded {self.value} for {self.listing.title}"
--- 2/commerce/auctions/views.py
+++ 2/commerce/auctions/views.py
@@ -4,11 +4,18 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing, Bid, Item
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.all()
+
+    
+
+
+    return render(request, "auctions/index.html", {
+        "listings": listings
+    })
 
 
 def login_view(request):
@@ -61,3 +68,22 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    if request.method == "GET":
+        return render(request, "auctions/create_listing.html")
+    
+    title = request.POST["title"]
+    description = request.POST["description"]
+    picture_link = request.POST["picture_link"]
+    category = request.POST["category"]
+    starting_bid = request.POST["starting_bid"]
+    user_id = request.user.id
+
+    user = User.objects.get(pk=user_id)
+
+    item = Item.objects.create(title=title, description=description, picture_link=picture_link, category=category, starting_bid=starting_bid)
+    Listing.objects.create(owner=user, item=item)
+
+    return HttpResponseRedirect(reverse("index"))

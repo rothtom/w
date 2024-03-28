533f1d5fcab921d00cc0c0affc9ca0a6529b5ee0
rothtom <t.roth1@outlook.com>
2024-03-28 16:38:58 +0100

    pushing files ive forgotten bevore?

--- 2/commerce/auctions/admin.py
+++ 2/commerce/auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Bid, Item, Listing, User, Comment
 # Register your models here.
+
+
+admin.site.register(Bid)
+admin.site.register(Item)
+admin.site.register(Listing)
+admin.site.register(User)
+admin.site.register(Comment)
